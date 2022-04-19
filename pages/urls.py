78d4e0209ff7d21d9fbf9343e0ea90c2050067fb
ahmadraja <ahmadraza001912@gmail.com>
2022-04-19 23:17:11 +0530

    first commit

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from pages import views
+urlpatterns = [
+  path("",views.home,name='home'),
+  path('about/',views.about, name='about'),
+  path('contact/',views.contact,name='contact'),
+  path('services/',views.services,name='services'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
